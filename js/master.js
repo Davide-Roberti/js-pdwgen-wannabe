01e94767bed231b25aff82d7d4add2d746c61059
Davide <davide.roberti89@gmail.com>
2020-02-17 15:20:21 +0100

    fine

---
+++ js/master.js
@@ -0,0 +1,4 @@
+var nome = prompt('Scrivi il tuo nome');
+var cognome = prompt('Scrivi il tuo cognome');
+var colore = prompt('Scrivi il tuo colore preferito');
+document.getElementById('password').innerHTML = nome + cognome + colore + 20;
