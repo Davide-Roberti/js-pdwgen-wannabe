151912ca5bc996c7513575241afd34858575af99
Davide <davide.roberti89@gmail.com>
2020-02-17 15:39:06 +0100

    prime due variazioni nei commenti

--- js/master.js
+++ js/master.js
@@ -2,3 +2,11 @@ var nome = prompt('Scrivi il tuo nome');
 var cognome = prompt('Scrivi il tuo cognome');
 var colore = prompt('Scrivi il tuo colore preferito');
 document.getElementById('password').innerHTML = nome + cognome + colore + 20;
+
+var data = new Date();
+var annoCorrente = data.getFullYear();
+//prima variazione con anno corrente
+// document.getElementById('password').innerHTML = nome + cognome + colore + annoCorrente;
+
+//seconda variazione con punto e virgola
+// document.getElementById('password').innerHTML = nome + ';' + cognome + ';' + colore + ';' + annoCorrente;
